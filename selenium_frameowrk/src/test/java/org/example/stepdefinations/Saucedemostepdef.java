3dfe5dc82323a4bf377ee02a0ad95f4667d5a64c
Rupak <rbhattacharjee26@yahoo.com>
2024-02-15 09:26:16 +0530

    added selenium bdd

--- selenium_frameowrk/src/test/java/org/example/stepdefinations/Saucedemostepdef.java
+++ selenium_frameowrk/src/test/java/org/example/stepdefinations/Saucedemostepdef.java
@@ -3,16 +3,43 @@ package org.example.stepdefinations;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Saucedemostepdef {
+    WebDriver driver;
 
-    @When("User enters {string}")
-    public void user_enters(String user_cred) {
-        System.out.println("user entered "+user_cred);
+
+    @Given("User initilize the browser")
+    public void user_initilize_the_browser() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com/");
     }
+
+    @When("User enters username {string}")
+    public void user_enters_username(String username) {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+
+    }
+    @When("User enters password {string}")
+    public void user_enters_password(String password) {
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
 
     @Then("User should be able to login")
     public void user_should_be_able_to_login() {
-        System.out.println("user successfully login");
+        String title = driver.getTitle();
+        System.out.println("The title of the website is: "+title);
+        driver.quit();
     }
 }
--- selenium_frameowrk/src/test/resources/features/simple.feature
+++ selenium_frameowrk/src/test/resources/features/simple.feature
@@ -2,6 +2,7 @@ Feature: All test cases for positive & negative scenarios
 
   @simpletest
   Scenario: Validate positive login flow
-    When User enters "standard_user"
-    And User enters "secret_sauce"
+    Given User initilize the browser
+    When User enters username "standard_user"
+    And User enters password "secret_sauce"
     Then User should be able to login
